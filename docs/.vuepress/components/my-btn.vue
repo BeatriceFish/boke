603935230736cd4a840a3d480ae35ad16fb24c5f
BeatriceFish <854787572@qq.com>
2021-08-20 14:37:04 +0800

    基本搭建完毕

---
+++ docs/.vuepress/components/my-btn.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="bandlClick">我是你爹</button>
+</template>
+
+<script>
+export default {
+    methods:{
+        bandlClick(){
+            alert("我是你爹")
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
